384c1aeb126b2e37e40cdfd6b50103c00d120e3d
Owen Mockler <omockler@salesforce.com>
2016-03-24 16:31:07 -0400

    In channel responses

--- app.rb
+++ app.rb
@@ -1,8 +1,14 @@
 require 'sinatra'
+require 'json'
 
 post '/choose' do
   halt 403 unless ENV["TOKEN"] == params["token"]
+  content_type :json
   items = params["text"]
   items = items.split(",")
-  items.sample
+  {
+    response_type:  "in_channel",
+    text: items.sample,
+    attachments: [{ "text": params["text"] }]
+  }.to_json
 end
